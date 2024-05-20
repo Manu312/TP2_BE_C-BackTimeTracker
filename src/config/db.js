78c25b3255f323fbaeda5c4cec16d4bbe030e8c6
Manu312 <manumercado14@gmail.com>
2024-05-20 20:02:53 -0300

    Agregamos logica de auth y la entidad User

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 // Configuración de la conexión a la base de datos
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root', //Completar
-  password: 'M@nukpo14',//Completar
+  user: '', //Completar
+  password: '',//Completar
   database: 'apptimetracker'
 });
 
