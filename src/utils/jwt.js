204793e6295269b9ebd85bc46413de589a88bd5b
Manu312 <manumercado14@gmail.com>
2024-06-10 23:18:42 -0300

    Agregamos example.env

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const SECRET_KEY = 'my_secret_jwt'; //TODO Cambiar por una clave mas segura
+const SECRET_KEY = process.env.JWT_SECRET || 'my_secret_jwt'; //TODO Cambiar por una clave mas segura
 
 class JwtUtils {
   static generateToken(payload) {
