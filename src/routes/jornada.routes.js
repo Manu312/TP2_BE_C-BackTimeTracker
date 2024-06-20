11e13b584c1daf6abe356a34fe24cc83d86b29c9
Ivan Parunov <ivanparu04@gmail.com>
2024-06-20 11:58:40 -0300

    Cambio nombre confuso

--- src/routes/jornada.routes.js
+++ src/routes/jornada.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const JornadaController = require("../controllers/jornada.controller");
-const validateUserRole = require("../middlewares/validateRoleUser");
+const validateRoleUSer = require("../middlewares/validateRoleUser");
 const validateUserAutenticated = require('../middlewares/validateUserAutenticated');
 
 router.post("/create", JornadaController.createJornada);
 router.get("/:idProject/jornadas", JornadaController.getAllJornadasByProject);
-router.delete("/:idProject/jornadas/:idJornada", validateUserAutenticated(), validateUserRole(["admin"]), JornadaController.deleteJornada);
+router.delete("/:idProject/jornadas/:idJornada", validateUserAutenticated(), validateRoleUSer(["admin"]), JornadaController.deleteJornada);
 
 module.exports = router;
